ecadcc29fc9a4eef9f95c7f598ee6b4ed8e0c373
Lamija <lamija.pehilj@gmail.com>
2022-12-20 09:35:06 +0100

    new additional configuration

--- 07-12-2022/wdio.conf.js
+++ 07-12-2022/wdio.conf.js
@@ -61,7 +61,7 @@ exports.config = {
         browserName: 'chrome',
         acceptInsecureCerts: true,
         "goog:chromeOptions": {
-        args: ["--no-sandbox","--disable-dev-shm-usage"]
+        args: ['--no-sandbox','--headless','--disable-dev-shm-usage','--disable-gpu'],
         }
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
